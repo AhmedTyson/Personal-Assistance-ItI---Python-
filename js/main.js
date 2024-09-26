fb4bb8c3b6210965db7537d6ebbea5f000e092ab
Ahmed Elsayed <Ahmed.Elsayed.ab.bis24@commerce.helwan.edu.eg>
2024-09-26 21:45:16 +0300

    Menu_list

--- js/main.js
+++ js/main.js
@@ -1,27 +1,28 @@
 const toggleButton = document.getElementById("mode-toggle");
 const modeIcon = document.getElementById("mode-icon");
 
+// Elements that will change in dark mode
+const darkModeElements = [document.body, document.querySelector(".navbar")];
+
 // Check for saved user preference for dark mode (if any)
 const userPrefersDark = localStorage.getItem("darkMode") === "enabled";
 
 // Apply saved preference on load
 if (userPrefersDark) {
-  document.body.classList.add("dark-mode");
-  document.querySelector(".navbar").classList.add("dark-mode");
-  modeIcon.src = "dark-icon.png"; // Change to dark icon
+  darkModeElements.forEach((el) => el.classList.add("dark-mode"));
+  modeIcon.src = "staticimages/dark-icon.png"; // Change to dark icon
 }
 
 // Toggle dark mode when button is clicked
 toggleButton.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-  document.querySelector(".navbar").classList.toggle("dark-mode");
+  darkModeElements.forEach((el) => el.classList.toggle("dark-mode"));
 
   // Toggle icon image and save the user's preference in localStorage
   if (document.body.classList.contains("dark-mode")) {
-    modeIcon.src = "dark-icon.png"; // Set dark mode icon
+    modeIcon.src = "staticimages/dark-icon.png"; // Set dark mode icon
     localStorage.setItem("darkMode", "enabled");
   } else {
-    modeIcon.src = "light-icon.png"; // Set light mode icon
+    modeIcon.src = "staticimages/light-icon.png"; // Set light mode icon
     localStorage.setItem("darkMode", "disabled");
   }
 });
